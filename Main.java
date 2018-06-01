0bcb373be9373a11d56e0c74e8f2960ac043dff0
Erik <erikhansson85@gmail.com>
2018-06-01 16:12:57 +0200

    changed java to Narnia and included a Scanner-object

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Narnia");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
 	}
 }
